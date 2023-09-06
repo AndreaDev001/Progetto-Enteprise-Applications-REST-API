1b98ea502631f74cd371e72bcd9bc0d9a6a3071f
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2023-09-06 14:13:05 +0200

    Refactoring nomi

--- src/main/java/com/enterpriseapplications/springboot/EnterpriseApplicationsSpringBootApplication.java
+++ src/main/java/com/enterpriseapplications/springboot/EnterpriseApplicationsSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.enterpriseapplications.progettoentepriseapplicationsspringboot;
+package com.enterpriseapplications.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class EnterpriseApplicationsSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnterpriseApplicationsSpringBootApplication.class, args);
     }
-
 }
